3a581a8c59c81df1b23004e4f63077f90a1995dc
Chhewang <chhewang.tmg@gmail.com>
2023-02-11 15:40:36 +0100

    initial commit

---
+++ src/main/java/com/tmg/xpenses/XpensesApplication.java
@@ -0,0 +1,13 @@
+package com.tmg.xpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmg/xpenses/XpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmg.xpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
